b8c0c5f5937035598401fdea50bed0d0320e7c4d
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2025-10-28 12:02:46 +0600

    3 sec delay

--- lib/services/hotel_api_service.dart
+++ lib/services/hotel_api_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<List<dynamic>> fetchHotels() async {
-  await Future.delayed(Duration(seconds: 1));
+  await Future.delayed(Duration(seconds: 3));
   try {
     final response = await http.get(Uri.parse('https://gist.githubusercontent.com/tanjil-dev/2b4db997964028d3e9c295daa1681647/raw/1cc1aed1d8791a4e1279434c32ebe5f9d490de4b/my-travel-agency-api'));
     if (response.statusCode == 200) {
